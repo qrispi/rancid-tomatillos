f72c76b092115ea2a38c8f27a4846da453d6d3d2
Christopher Baum <christopherabaum@gmail.com>
2023-03-23 18:24:39 -0500

    Initialize Poster and set View to generate all poster images

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -1,10 +1,11 @@
 import './View.css';
 import React from 'react';
+import Poster from '../Poster/Poster';
 
 const View = ({movies}) => {
     return (
-        <section>
-            {movies.length > 1 ? <h1>Multiple Movies</h1> : <h1>One Movie</h1>}
+        <section className=''>
+            {movies.length > 1 ? <div className='poster-container'>{movies.map(movie => <Poster key={movie.id} data={movie} />)}</div> : <h1>One Movie</h1>}
         </section>
     )
 }
