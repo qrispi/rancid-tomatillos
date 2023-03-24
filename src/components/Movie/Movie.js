b11d024bed9df14c26a386fbbd0193e7b8510eed
Amber Shipley <ashipley2@me.com>
2023-03-24 16:44:08 -0700

    Create basic structure for child componenets of Movie and minimal styling

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,13 +1,16 @@
 import React from "react";
 import './Movie.css';
 import Hero from "../Hero/Hero";
+import Tagline from "../Tagline/Tagline";
 import Media from "../Media/Media";
 import MovieDescription from "../MovieDescription/MovieDescription";
 
 const Movie = ({data, clearSelectedMovie}) => {
     return (
-        <section>
+        <section className='Movie'>
             <Hero info={data}/>
+            <MovieDescription info={data} />
+            {data.genres.map(tagline => <Tagline key={Date.now()} tagline={tagline}/>)}
             <button onClick={clearSelectedMovie}>Go Back</button>
         </section>
     )
