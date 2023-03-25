404f42b11f782ded99772be239ea64f3d43e0dc0
Amber Shipley <ashipley2@me.com>
2023-03-25 15:47:06 -0700

    Update styling for MovieDescription and add coditional rendering

--- src/components/MovieDescription/MovieDescription.js
+++ src/components/MovieDescription/MovieDescription.js
@@ -2,16 +2,16 @@ import './MovieDescription.css';
 
 const MovieDescription = ({info}) => {
     return (
-        <div>
+        <div className='MovieDescription'>
             <div>
-                <h3>{info.tagline}</h3>
-                <p>{info.overview}</p>
+                {info.tagline && <h3 className='Tagline'>{info.tagline}</h3>}
+                {info.overview && <p className='Overview'>{info.overview}</p>}
             </div>
-            <div>
-                <h3>Stats</h3>
-                <p>Budget: {info.budget}</p>
-                <p>Revenue: {info.revenue}</p>
-                <p>Runtime: {info.runtime} minutes</p>
+            <div className='Stats'>
+                {info.budget && info.revenue && info.runtime ? <h3>Stats:</h3> : <h3>Sorry, no stats</h3>}
+                {info.budget && <p>Budget: ${info.budget}</p>}
+                {info.revenue && <p>Revenue: ${info.revenue}</p>}
+                {info.runtime && <p>Runtime: {info.runtime} minutes</p>}
             </div>
         </div>
     )
