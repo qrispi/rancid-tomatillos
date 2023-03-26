52944bc21f8de3502900c3ad86b3362d23852870
Amber Shipley <ashipley2@me.com>
2023-03-25 17:22:11 -0700

    Only show 3 videos total in Media section

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -1,10 +1,8 @@
 import './Media.css';
 
 const Media = ({videos}) => {
-    console.log(videos)
     const videoKeys = videos.filter(video => video.type !== 'Trailer').map(video => video.key)
     const trailer = videos.find(video => video.type === "Trailer")
-    console.log(videoKeys)
 
     return (
         <section className='Media'>
@@ -13,11 +11,9 @@ const Media = ({videos}) => {
             </div>
             <div className='Multiple-videos'>
 
-                {videoKeys[1] && <iframe className='Small-video' src={`https://www.youtube.com/embed/${videoKeys[1]}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>}
+                {videoKeys[0] && <iframe className='Small-video' src={`https://www.youtube.com/embed/${videoKeys[1]}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>}
 
-                {videoKeys[2] && <iframe className='Small-video' src={`https://www.youtube.com/embed/${videoKeys[2]}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>}
-
-                {/* {videoKeys[3] && <iframe className='Small-video' src={`https://www.youtube.com/embed/${videoKeys[3]}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>} */}
+                {videoKeys[1] && <iframe className='Small-video' src={`https://www.youtube.com/embed/${videoKeys[2]}`} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>}
             </div>
         </section>
     )
