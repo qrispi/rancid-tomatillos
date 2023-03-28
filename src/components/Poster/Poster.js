a8fad179f246ce1c15644afb60926d04281a00d3
Christopher Baum <christopherabaum@gmail.com>
2023-03-28 18:11:12 -0500

    Add prop types and a default poster_path to Poster

--- src/components/Poster/Poster.js
+++ src/components/Poster/Poster.js
@@ -12,3 +12,18 @@ const Poster = ({data, fetchSingleMovie, error}) => {
 }
 
 export default Poster;
+
+Poster.propTypes = {
+    data: PropTypes.shape({
+        poster_path: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired
+    }).isRequired,
+    fetchSingleMovie: PropTypes.func.isRequired,
+    error: PropTypes.string
+}
+
+Poster.defaultProps = {
+    data: {
+        poster_path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfptVpQVR23mAxQ-SKqqRusGUaaiI98whu9w&usqp=CAU'
+    }
+}
