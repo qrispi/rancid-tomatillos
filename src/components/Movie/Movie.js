cf78b0b0dc5088153ddd8fb47b64d16535e681e4
Christopher Baum <christopherabaum@gmail.com>
2023-03-28 17:30:56 -0500

    Add styling for back button and hero background image

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -7,7 +7,7 @@ import MovieDescription from "../MovieDescription/MovieDescription";
 const Movie = ({data, clearSelectedMovie, videos}) => {
     return (
         <section className='Movie'>
-            <button onClick={clearSelectedMovie}>Go Back</button>
+            <button className='back-button' onClick={clearSelectedMovie}>Go Back</button>
             <Hero info={data}/>
             <MovieDescription info={data} />
             {videos && <Media videos={videos} />}
