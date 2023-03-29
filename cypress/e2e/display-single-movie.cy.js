b68a8475ced9e43105ddeb12cc49300279623035
Amber Shipley <ashipley2@me.com>
2023-03-28 18:43:33 -0700

    Make sure a different movie can be viewed

--- cypress/e2e/display-single-movie.cy.js
+++ cypress/e2e/display-single-movie.cy.js
@@ -12,4 +12,14 @@ describe('user should be able to see details for a specific movie', () => {
     cy.get('.Stats')
       .contains('p', 'Runtime: 106 min')
   });
+
+  it('should be able to click a different movie and see that specific movie\'s details', () => {
+    cy.get('.poster').first().click()
+    cy.get('.Hero')
+      .contains('h2', 'Black Adam')
+    cy.get('.Movie-info')
+      .contains('h3', 'The world needed a hero. It got Black Adam.')
+    cy.get('.Stats')
+      .contains('p', 'Runtime: 125 min')
+  });
 })
