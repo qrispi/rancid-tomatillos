bc2ae90de61a745bf87b744c11c5e56955a0b938
Christopher Baum <christopherabaum@gmail.com>
2023-03-30 17:58:12 -0500

    Remove extraneous code from Movie

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -4,7 +4,7 @@ import './Movie.css';
 import Hero from "../Hero/Hero";
 import Media from "../Media/Media";
 import MovieDescription from "../MovieDescription/MovieDescription";
-import { Route, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class Movie extends Component {
     constructor() {
@@ -16,7 +16,6 @@ class Movie extends Component {
         }
     }
     componentDidMount = () => {
-        console.log("MOUNTED")
         fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}`)
           .then(response => {
             if(!response.ok) {
@@ -28,31 +27,24 @@ class Movie extends Component {
           }
         ).then(data => {
           this.setState({selectedMovie: data.movie});
-          console.log("THE STATE IS SET")
         });
         fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}/videos`)
           .then(response => {
             if(!response.ok) {
-              this.setState({videosError: response.status})
+              this.setState({videosError: response.status});
             } else {
               return response.json();
             }
           }
         ).then(data => {
           this.setState({selectedMovieVideos: data.videos});
-          this.setState({mounted: true})
         });
     }
-    unmount = () => {
-        console.log("I GOT CLICKED")
-        this.setState({mounted: false});
-    }
     render() {
-        console.log(this.state)
         return (
             <section className='Movie'>
                 <NavLink to='/'>
-                    <button onClick={() => this.unmount()}>Go Back</button>
+                    <button>Go Back</button>
                 </NavLink>
                 {this.state.selectedMovie && <Hero info={this.state.selectedMovie}/>}
                 {this.state.selectedMovie && <MovieDescription info={this.state.selectedMovie} />}
