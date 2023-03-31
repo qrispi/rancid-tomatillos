ba0de509d67c2f1a9bf932d27493b2d488a5bdc2
Amber Shipley <ashipley2@me.com>
2023-03-31 15:53:47 -0700

    Change css class names to lowercase

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,7 @@ import "./Hero.css";
 
 const Hero = ({info}) => {
     return (
-        <div className='Hero' style={{ backgroundImage:`url(${info.backdrop_path})`}}>
+        <div className='hero' style={{ backgroundImage:`url(${info.backdrop_path})`}}>
             <h2 className='opaque-background'>{info.title}</h2>
             <p className='opaque-background' >Release Year: {info.release_date}</p>
             <p className='opaque-background' >Rating: {info.average_rating}</p>
