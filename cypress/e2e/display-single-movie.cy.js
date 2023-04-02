3ac1d235db056d1054e88f0baa38daae7e581acd
Christopher Baum <christopherabaum@gmail.com>
2023-04-02 16:41:18 -0500

    Fix first two failing cypress tests

--- cypress/e2e/display-single-movie.cy.js
+++ cypress/e2e/display-single-movie.cy.js
@@ -11,22 +11,20 @@ describe('user should be able to see details for a specific movie', () => {
 
   it('should be able to click a movie and see that specific movie\'s details', () => {
   
-    cy.get('.poster').last().click()
-    cy.get('.Hero')
-      .contains('h2', 'Black Panther: Wakanda Forever')
-    cy.get('.Movie-info')
-      .contains('h3', 'Forever.')
-    cy.get('.Stats')
+    cy.get('.poster-img').last().click()
+    cy.get('.hero')
+    cy.contains('Black Panther: Wakanda Forever')
+    cy.contains('Forever.')
+    cy.get('.stats')
       .contains('p', 'Runtime: 162 min')
   });
 
   it('should be able to click a different movie and see that specific movie\'s details', () => {
-    cy.get('.poster').first().click()
-    cy.get('.Hero')
-      .contains('h2', 'Black Adam')
-    cy.get('.Movie-info')
-      .contains('h3', 'The world needed a hero. It got Black Adam.')
-    cy.get('.Stats')
+    cy.get('.poster-img').first().click()
+    cy.get('.hero')
+    cy.contains('Black Adam')
+    cy.contains('The world needed a hero. It got Black Adam.')
+    cy.get('.stats')
       .contains('p', 'Runtime: 125 min')
   });
 })
