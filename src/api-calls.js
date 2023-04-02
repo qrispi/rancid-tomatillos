e53fa672f61cca6c097028e5dfef64681e6cdba1
Amber Shipley <ashipley2@me.com>
2023-04-02 13:52:23 -0700

    Add missing semicolon

--- src/api-calls.js
+++ src/api-calls.js
@@ -2,7 +2,7 @@ export default (path) => {
     return fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/${path}`)
       .then(response => {
         if(!response.ok) {
-          throw new Error(response.status)
+          throw new Error(response.status);
         } else {
           return response.json();
         }
