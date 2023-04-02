bf913daaa0db4ef7b657ceba1d7f4c23f2760023
Christopher Baum <christopherabaum@gmail.com>
2023-04-02 11:17:25 -0500

    Import api-calls to Movie and reconfigure componentDidMount to use the imported function

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -4,6 +4,7 @@ import './Movie.css';
 import Hero from "../Hero/Hero";
 import Media from "../Media/Media";
 import MovieDescription from "../MovieDescription/MovieDescription";
+import fetchData from '../../api-calls';
 
 class Movie extends Component {
     constructor() {
@@ -15,29 +16,39 @@ class Movie extends Component {
         }
     }
     componentDidMount = () => {
-        fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}`)
-          .then(response => {
-            if(!response.ok) {
-              this.setState({singleMovieError: [response.status, this.props.movieId]});
-              setTimeout(() => this.setState({singleMovieError: []}), 4000);
-            } else {
-              return response.json();
-            }
-          }
-        ).then(data => {
-          this.setState({selectedMovie: data.movie});
-        });
-        fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}/videos`)
-          .then(response => {
-            if(!response.ok) {
-              this.setState({videosError: response.status});
-            } else {
-              return response.json();
-            }
-          }
-        ).then(data => {
-          this.setState({selectedMovieVideos: data.videos});
-        });
+        fetchData(`movies/${this.props.movieId}`).then(data => {
+            this.setState({selectedMovie: data.movie});
+        }).catch(error => {
+            // this.props.logError()
+        })
+        fetchData(`movies/${this.props.movieId}/videos`).then(data => {
+            this.setState({selectedMovieVideos: data.videos});
+        }).catch(error => {
+            console.log(error);
+        })
+        // fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}`)
+        //   .then(response => {
+        //     if(!response.ok) {
+        //       this.setState({singleMovieError: [response.status, this.props.movieId]});
+        //       setTimeout(() => this.setState({singleMovieError: []}), 4000);
+        //     } else {
+        //       return response.json();
+        //     }
+        //   }
+        // ).then(data => {
+        //   this.setState({selectedMovie: data.movie});
+        // });
+        // fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.movieId}/videos`)
+        //   .then(response => {
+        //     if(!response.ok) {
+        //       this.setState({videosError: response.status});
+        //     } else {
+        //       return response.json();
+        //     }
+        //   }
+        // ).then(data => {
+        //   this.setState({selectedMovieVideos: data.videos});
+        // });
     }
     render() {
         return (
